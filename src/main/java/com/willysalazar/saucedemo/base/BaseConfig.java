8e19be7bcd67d3ba44bf849b32a1e7714b17c1eb
willysalazar <willysal@gmail.com>
2024-07-13 17:40:04 -0300

    :fire: remove the internet examples and classes

--- src/main/java/com/willysalazar/saucedemo/base/BaseConfig.java
+++ src/main/java/com/willysalazar/saucedemo/base/BaseConfig.java
@@ -1,11 +1,15 @@
-package com.willysalazar.base;
+package com.willysalazar.saucedemo.base;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import com.willysalazar.config.ServerConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.*;
 
 public abstract class BaseConfig {
 
@@ -16,8 +20,16 @@ public abstract class BaseConfig {
                         .savePageSource(true));
     }
 
+    @BeforeEach
+    public void preConditions(){
+        ServerConfig serverConfig = ConfigFactory.create(ServerConfig.class);
+        Configuration.browser = "firefox";
+        open(serverConfig.baseUrlSauceDemo());
+    }
+
     @AfterAll
     public static void logout() {
+        clearBrowserCookies();
         closeWebDriver();
     }
 }
--- src/test/java/com/willysalazar/saucedemo/AddItemToCartTest.java
+++ src/test/java/com/willysalazar/saucedemo/AddItemToCartTest.java
@@ -1,17 +1,16 @@
 package com.willysalazar.saucedemo;
 
 import com.codeborne.selenide.CollectionCondition;
-import com.willysalazar.saucedemo.base.BaseConfigSauceDemo;
+import com.willysalazar.saucedemo.base.BaseConfig;
 import com.willysalazar.saucedemo.pageobject.CartPageObject;
 import com.willysalazar.saucedemo.pageobject.InventoryPageObject;
 import com.willysalazar.saucedemo.pageobject.LoginPageObject;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 @Feature("Add items to cart")
-class AddItemToCartTest extends BaseConfigSauceDemo {
+class AddItemToCartTest extends BaseConfig {
 
     @Test
     @Description("Should add backpack to cart and cart size to be one")
